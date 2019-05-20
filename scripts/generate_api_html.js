f0a6d75196c64fe9bc4c7febee6fe7de30a00459
Jen Dewalt <jen.dewalt@gmail.com>
2019-05-20 16:26:27 -0700

    Fill in and style param options tables

--- scripts/generate_api_html.js
+++ scripts/generate_api_html.js
@@ -21,8 +21,25 @@ ejs.renderFile(gettingStartedPath, apiData.GettingStarted, (err, html) => {
     gettingStartedBlock = html;
 });
 
+const tablePath = path.join(__dirname, '../templates/api/table.ejs');
+let endpointData = {};
+endpointData.sections = apiData.EndpointSections;
+endpointData.sections.forEach(section => {
+    section.endpoints.forEach(endpoint => {
+        endpoint.paramsTables = '';
+        endpoint.params.forEach(param => {
+            const header = Object.keys(param)[0];
+            const options = param[header];
+            ejs.renderFile(tablePath, { header, options }, (err, html) => {
+                if (err) console.log(err);
+                endpoint.paramsTables += html;
+            });
+        });
+    });
+});
+
 const endpointsPath = path.join(__dirname, '../templates/api/endpoints.ejs');
-ejs.renderFile(endpointsPath, apiData, (err, html) => {
+ejs.renderFile(endpointsPath, endpointData, (err, html) => {
     if (err) console.log(err);
     endpointsBlock = html;
 });
